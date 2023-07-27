ca23228f48c79fabc30479868f9a1dca7587b4d9
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-27 02:19:14 +0200

    add data fetching add edit styles

--- src/components/Offerings.js
+++ src/components/Offerings.js
@@ -1,14 +1,41 @@
+"use client";
+
 import Image from "next/image";
 import recognition from "../../public/icon-brand-recognition.svg";
 import records from "../../public/icon-detailed-records.svg";
 import customizable from "../../public/icon-fully-customizable.svg";
 import Form from "./Form";
+import { useState } from "react";
+import UrlList from "./UrlList";
 
 const Offerings = () => {
+  const [urlList, setUrlList] = useState([]);
+
+  const getShortenUrl = async (url) => {
+    const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    const data = await response.json();
+    setUrlList((urlList) => [
+      {
+        id: data.result.code,
+        original: data.result.original_link,
+        short: data.result.short_link,
+      },
+      ...urlList,
+    ]);
+  };
+
   return (
     <div className="relative flex justify-center">
-      <Form />
+      <Form getShortenUrl={getShortenUrl} />
       <article className="bg-gray-100 py-24 px-5 w-full">
+        <div className="flex justify-center">
+          <UrlList urlList={urlList} />
+        </div>
         <div className="flex flex-col gap-5 pb-24">
           <h1 className="font-bold text-4xl md:text-5xl text-darkViolet text-center px-12">
             Advanced Statistics
@@ -19,8 +46,8 @@ const Offerings = () => {
           </p>
         </div>
         <div>
-          <div className="flex flex-col gap-20 justify-center items-center lg:flex-row lg:py-24 lg:h-96">
-            <div className="bg-cyan w-2 lg:h-4/5 absolute flex h-3/5 lg:rotate-90"></div>
+          <div className="flex flex-col gap-20 justify-center items-center lg:flex-row lg:py-24 lg:h-96 relative">
+            <div className="bg-cyan w-2 h-[900px] absolute lg:h-2 lg:w-[1000px]"></div>
             <div className="bg-white mx-5 pt-12 pb-10 px-7 relative flex items-center lg:self-end lg:items-start flex-col rounded-lg max-w-sm">
               <div className="bg-darkViolet w-24 h-24 flex justify-center items-center rounded-full absolute -top-10 lg:left-5 lg:-top-11">
                 <Image src={recognition} alt="recognition images" />
