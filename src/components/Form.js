9f2a57a021a12a7950ac6cfe4167df2ee9a9cb6d
Bartosz Sakiewa <barsak1177@gmail.com>
2023-07-27 02:19:01 +0200

    edit some styles, add function onFormSubmit

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,31 +2,55 @@
 
 import Image from "next/image";
 import background from "../../public/bg-shorten-desktop.svg";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-const Form = () => {
-  const [link, setLink] = useState("");
+const Form = ({ getShortenUrl }) => {
+  const [url, setUrl] = useState("");
+  const [isEmpty, setIsEmpty] = useState(false);
+  const inputRef = useRef();
 
   const onFormSubmit = (e) => {
     e.preventDefault();
-    
+
+    if (url === "") {
+      setIsEmpty(true);
+      inputRef.current.focus();
+      return;
+    }
+
+    if (!url.includes("https" || "http")) {
+      setIsEmpty(true);
+      inputRef.current.focus();
+      return;
+    }
+
+    setIsEmpty(false);
+    getShortenUrl(url);
+    setUrl("");
   };
 
   return (
-    <form onSubmit={onFormSubmit} className="absolute -top-28">
+    <form onSubmit={onFormSubmit} className="absolute -top-28 mx-5">
       <Image
         src={background}
         alt="form background"
         className="bg-darkViolet h-48 rounded-md"
       />
-      <div className="absolute z-50 top-8 min-[533px]:top-16 flex flex-wrap px-12 gap-4 w-full">
+      <div className="absolute z-50 top-8 min-[573px]:top-16 flex flex-wrap px-12 gap-4 w-full">
         <input
+          ref={inputRef}
           placeholder="Shorten a link here..."
-          className="py-4 px-5 rounded-md grow"
-          required
-          value={link}
-          onChange={({ target }) => setLink(target.value)}
+          className={`py-4 px-5 rounded-md grow ${
+            isEmpty ? "placeholder:text-red outline outline-3 outline-red" : ""
+          }`}
+          value={url}
+          onChange={({ target }) => setUrl(target.value)}
         />
+        <p
+          className={`text-red absolute top-16 ${isEmpty ? "block" : "hidden"}`}
+        >
+          Please add a link!!!
+        </p>
         <button className="bg-cyan text-white py-4 px-10 rounded-md hover:brightness-150 duration-200 grow sm:grow-0">
           Shorten It!
         </button>
